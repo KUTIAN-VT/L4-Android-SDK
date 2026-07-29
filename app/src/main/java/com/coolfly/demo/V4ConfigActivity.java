09d493292e7b3b2fa30fdaf269ab810f47289083
wuzh <contact@zongheng.pro>
2026-07-29 14:14:06 +0800

    Upgrade libraries. Eth improvement. FFmpeg 8.1.2.

--- app/src/main/java/com/coolfly/demo/V4ConfigActivity.java
+++ app/src/main/java/com/coolfly/demo/V4ConfigActivity.java
@@ -63,7 +63,7 @@ public class V4ConfigActivity extends AppCompatActivity {
 
         binding.tvReset.setOnClickListener(v -> {
             protocolHelper.ar8030ResetConfigJson(binding.swRemote.isChecked());
-            protocolHelper.ar8030ResetMiniDb();
+            protocolHelper.ar8030ResetMiniDb(binding.swRemote.isChecked());
         });
 
         binding.tvSave.setOnClickListener(v -> {
